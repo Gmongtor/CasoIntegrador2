b9e1835b763348a0a1160e6cf3f2d4044261df12
Gmongtor <gmongtor@myuax.com>
2024-03-06 11:37:00 +0100

    creamos la clase animales para poder extenderla a clases mas especificas como terrestre aviario o acuatico

--- untitled/src/main/java/Animales/Animales.java
+++ untitled/src/main/java/Animales/Animales.java
@@ -1,4 +1,49 @@
 package Animales;
 
-public class Animales {
+public class Terrestre extends Animales {
+    private String tipoDeLocomocion;
+    private String ultimaComida;
+    private String estadoAnimo;
+
+    public Terrestre(String nombre, int edad, String estadoSalud, String tipoDeLocomocion) {
+        super(nombre, edad, estadoSalud);
+        this.tipoDeLocomocion = tipoDeLocomocion;
+        this.ultimaComida = "";
+        this.estadoAnimo = "Neutral";
+    }
+
+    @Override
+    public void alimentar(String comida) {
+        this.ultimaComida = comida;
+        System.out.println(nombre + " ha sido alimentado con " + comida + ".");
+    }
+
+    public void registrarSalud(String estadoSalud) {
+        this.estadoSalud = estadoSalud;
+        System.out.println("El estado de salud de " + nombre + " ha sido actualizado a: " + estadoSalud + ".");
+    }
+
+    public void registrarComportamiento(String comportamiento) {
+        this.estadoAnimo = comportamiento;
+        System.out.println("El comportamiento de " + nombre + " ha sido registrado como: " + comportamiento + ".");
+    }
+
+    @Override
+    public void sonidoEspecifico() {
+        System.out.println(nombre + " emite un sonido característico de su especie.");
+    }
+
+    public void mover() {
+        System.out.println(nombre + " se mueve usando su " + tipoDeLocomocion + ".");
+    }
+
+    // Getters y setters adicionales aquí
+
+    public String getUltimaComida() {
+        return ultimaComida;
+    }
+
+    public String getEstadoAnimo() {
+        return estadoAnimo;
+    }
 }
