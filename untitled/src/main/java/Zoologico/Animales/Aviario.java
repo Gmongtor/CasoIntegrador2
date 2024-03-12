50f32ef40038ecb7b3c141f436a0af4a10ae5178
Gmongtor <gmongtor@myuax.com>
2024-03-12 20:40:09 +0100

    Poco a poco

--- untitled/src/main/java/Zoologico/Animales/Aviario.java
+++ untitled/src/main/java/Zoologico/Animales/Aviario.java
@@ -1,8 +1,8 @@
 package Zoologico.Animales;
 
 public class Aviario extends Animales {
-    private String tipoDeVuelo;
-    private String canto;
+    private String tipoDeVuelo; // Ejemplo: "Planeador", "Aleteo rápido"
+    private String canto; // Ejemplo: "Trino melodioso", "Cacareo fuerte"
 
     public Aviario(String nombre, int edad, String estadoSalud, String tipoDeVuelo, String canto) {
         super(nombre, edad, estadoSalud);
@@ -19,15 +19,13 @@ public class Aviario extends Animales {
         System.out.println(nombre + " vuela utilizando un " + tipoDeVuelo + " tipo de vuelo.");
     }
 
-    // Implementación de métodos para registrar la alimentación, salud y comportamiento específicos de aves.
     @Override
     public void alimentar(String comida) {
         super.alimentar(comida);
-        // Añadir condiciones o comportamientos específicos de alimentación de aves aquí.
         System.out.println(nombre + " pica su comida: " + comida + ".");
     }
 
-    // Métodos getters y setters para tipoDeVuelo y canto aquí
+    // Getters y Setters
     public String getTipoDeVuelo() {
         return tipoDeVuelo;
     }
@@ -42,5 +40,27 @@ public class Aviario extends Animales {
 
     public void setCanto(String canto) {
         this.canto = canto;
+    }
+}
+class ZoologicoAviario {
+    public static void main(String[] args) {
+        Aviario aguila = new Aviario("Águila", 5, "Excelente", "Planeador", "Grito agudo");
+        Aviario colibri = new Aviario("Colibrí", 2, "Buena", "Aleteo rápido", "Zumbido suave");
+        Aviario loro = new Aviario("Loro", 10, "Buena", "Vuelo versátil", "Imitación de sonidos");
+
+        mostrarInformacionAviario(aguila);
+        mostrarInformacionAviario(colibri);
+        mostrarInformacionAviario(loro);
+    }
+
+    private static void mostrarInformacionAviario(Aviario ave) {
+        System.out.println("Nombre: " + ave.getNombre());
+        ave.sonidoEspecifico();
+        ave.volar();
+        System.out.println("Edad: " + ave.getEdad() + " años");
+        System.out.println("Estado de Salud: " + ave.getEstadoSalud());
+        System.out.println("Tipo de Vuelo: " + ave.getTipoDeVuelo());
+        System.out.println("Canto: " + ave.getCanto());
+        System.out.println("-----------------------------------------");
     }
 }
