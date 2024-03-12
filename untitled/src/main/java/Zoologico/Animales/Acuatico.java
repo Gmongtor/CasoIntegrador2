9df1a1231e2ff47e7aec195009f66e0a0ddbd7ce
Gmongtor <gmongtor@myuax.com>
2024-03-12 19:57:13 +0100

    Poco a poco

--- untitled/src/main/java/Zoologico/Animales/Acuatico.java
+++ untitled/src/main/java/Zoologico/Animales/Acuatico.java
@@ -31,5 +31,41 @@ public class Acuatico extends Animales {
     }
 }
 
+ class ZoologicoAcuatico {
+
+    public static void main(String[] args) {
+        // Creación de instancias de animales acuáticos distintos
+        Acuatico delfin = new Acuatico("Delfin", 10, "Buena", "Agua Salada", "Saltar fuera del agua");
+        Acuatico tiburon = new Acuatico("Tiburon", 15, "Excelente", "Agua Salada", "Nadar profundo");
+        Acuatico pezPayaso = new Acuatico("Pez Payaso", 2, "Buena", "Agua Salada", "Esconderse entre anémonas");
+        Acuatico tortuga = new Acuatico("Tortuga Marina", 50, "Buena", "Agua Salada", "Larga vida");
+        // Nuevas instancias
+        Acuatico mantaRaya = new Acuatico("Manta Raya", 5, "Buena", "Agua Salada", "Planea en el agua");
+        Acuatico pulpo = new Acuatico("Pulpo", 3, "Buena", "Agua Salada", "Cambio de color y forma");
+        Acuatico medusa = new Acuatico("Medusa", 1, "Buena", "Agua Salada", "Natación pulsátil");
+
+        // Mostrar información de cada animal acuático
+        mostrarInformacionAnimalAcuatico(delfin);
+        mostrarInformacionAnimalAcuatico(tiburon);
+        mostrarInformacionAnimalAcuatico(pezPayaso);
+        mostrarInformacionAnimalAcuatico(tortuga);
+        // Mostrar los nuevos animales
+        mostrarInformacionAnimalAcuatico(mantaRaya);
+        mostrarInformacionAnimalAcuatico(pulpo);
+        mostrarInformacionAnimalAcuatico(medusa);
+    }
+
+    private static void mostrarInformacionAnimalAcuatico(Acuatico animal) {
+        System.out.println("Nombre: " + animal.getNombre());
+        animal.sonidoEspecifico();
+        animal.nadar();
+        System.out.println("Edad: " + animal.getEdad() + " años");
+        System.out.println("Estado de Salud: " + animal.getEstadoSalud());
+        System.out.println("Habitat: " + animal.getTipoHabitat());
+        System.out.println("Habilidad Especial: " + animal.getHabilidadEspecial());
+        System.out.println("-----------------------------------------");
+    }
+}
+
 
 
